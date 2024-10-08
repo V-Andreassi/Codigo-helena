29cd586b54724e6caf1eec15c1df1d684c4b3eee
Davi-creator <davioliver2109@gmail.com>
2024-10-08 16:19:26 -0300

    modified:   Trabalho_Web/script/cadastro.js 	new file:   Trabalho_Web/script/cadastro_endereco.js 	modified:   Trabalho_Web/script/login.js

--- Trabalho_Web/script/cadastro.js
+++ Trabalho_Web/script/cadastro.js
@@ -31,6 +31,9 @@ async function cadastro(){
         alert('Por favor, preencha o campo Data de Nascimento.');
         
     }
+    
+    let user = JSON.parse(localStorage.getItem("user"));
+    console.log(user.acess_token);
 
    
 
@@ -48,8 +51,11 @@ async function cadastro(){
             "birthday":birthday   
         }),
         headers:{
-            'Content-Type':'application/json'
+            'Content-Type':'application/json',
+            'Cookie':'gowash_session=0hGqRHf0q38ETNgEcJGce30LcPtuPKo48uKtb7Oj',    
+            'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXBpLWdvLXdhc2gtZWZjOWM5NTgyNjg3Lmhlcm9rdWFwcC5jb20vYXBpL2xvZ2luIiwiaWF0IjoxNzEwNDE3MjIyLCJuYmYiOjE3MTA0MTcyMjIsImp0aSI6InBsZll0aENEZ0U1NUNzMHEiLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.z1pdEBkx8Hq01B7jNKa42NGxtFFHwb-7O_0y8krVWUY'
         }
+
     })
 
     if(api.ok){
